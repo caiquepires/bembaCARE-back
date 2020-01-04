7e2982c73855e631a7965224e6ea488ccab9e23c
Caique Pires <cfapires@gmail.com>
2020-01-03 22:13:08 -0300

    Arquivos iniciais do projeto

---
+++ src/test/java/com/dev/bemba/BembaCareBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.bemba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BembaCareBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
