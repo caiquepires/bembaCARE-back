cb95fa0ea9a770c09d092066580d2200728bd358
Caique Pires <cfapires@gmail.com>
2020-01-21 20:34:52 -0300

    Adicionando JPA e MYSQL ao projeto 2

--- src/main/java/com/dev/bemba/BembaCareBackApplication.java
+++ src/main/java/com/dev/bemba/BembaCareBackApplication.java
@@ -1,10 +1,19 @@
 package com.dev.bemba;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BembaCareBackApplication {
+	
+	@PostConstruct
+    void started() {
+      TimeZone.setDefault(TimeZone.getTimeZone("TimeZone"));
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(BembaCareBackApplication.class, args);
